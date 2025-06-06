059d9ac5e39e1ec03521dac2e5719db505d1fac4
Looyyd <filip.a.niedzielski@gmail.com>
2025-06-06 11:49:11 +0200

    trying bigger model

--- verifiers/examples/demo_frozenlake_train.py
+++ verifiers/examples/demo_frozenlake_train.py
@@ -28,13 +28,13 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python verifiers/inference/vllm_serve.py --model  '
 CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --num-processes 4 --config-file configs/zero3.yaml verifiers/examples/demo_frozenlake_train.py
 """
 
-# model_name = "Qwen/Qwen2.5-7B-Instruct"
-model_name = "Qwen/Qwen2.5-1.5B-Instruct"
-#model_name = "Qwen/Qwen2.5-0.5B-Instruct"
+model_name = "Qwen/Qwen2.5-7B-Instruct"
+# model_name = "Qwen/Qwen2.5-1.5B-Instruct"
+# model_name = "Qwen/Qwen2.5-0.5B-Instruct"
 
 # Configuration options
 IS_SLIPPERY = False  # Set to True for more challenging environment
-BATCH_SIZE = 4
+BATCH_SIZE = 16
 NUM_GENERATIONS = BATCH_SIZE
 N_INITIAL_SAMPLES = 1000  # Number of initial states in dataset
 FORMAT_REWARD_WEIGHT = 1.0  # Weight for format correctness
@@ -60,8 +60,8 @@ training_args = GRPOConfig(
     # Config recommended for Qwen 3 thinking, it's probably a good default for thinking tasks
     # TODO: i think it's maybe not good, because reduces variety during training, so less exploration
     temperature=1,
-    #top_p=0.95,
-    #top_k=20,
+    # top_p=0.95,
+    # top_k=20,
     min_p=0.0,
     max_steps=1000,
     bf16=True,
