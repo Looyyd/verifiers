80010bb83b302b9650cc91d591395d4a8170c42c
Looyyd <filip.a.niedzielski@gmail.com>
2025-06-07 11:23:38 +0200

    config for small model

--- verifiers/examples/demo_connectfour_train.py
+++ verifiers/examples/demo_connectfour_train.py
@@ -7,12 +7,12 @@ import bitsandbytes as bnb
 from verifiers.trainers.grpo_connectfour_trainer import GRPOConnectFourTrainer
 
 
-model_name = "Qwen/Qwen2.5-7B-Instruct"
-# model_name = "Qwen/Qwen2.5-1.5B-Instruct"
+# model_name = "Qwen/Qwen2.5-7B-Instruct"
+model_name = "Qwen/Qwen2.5-1.5B-Instruct"
 # model_name = "Qwen/Qwen2.5-0.5B-Instruct"
 
 # Configuration options
-BATCH_SIZE = 4
+BATCH_SIZE = 8
 NUM_GENERATIONS = BATCH_SIZE
 N_INITIAL_SAMPLES = 1000  # Number of initial states in dataset
 FORMAT_REWARD_WEIGHT = 1.0  # Weight for format correctness
@@ -32,9 +32,7 @@ training_args = GRPOConfig(
     learning_rate=1e-6,
     lr_scheduler_type="constant",
     num_train_epochs=1,
-    # Config recommended for Qwen 3 thinking, it's probably a good default for thinking tasks
-    # TODO: i think it's maybe not good, because reduces variety during training, so less exploration
-    temperature=1,
+    temperature=0.9,
     # top_p=0.95,
     # top_k=20,
     min_p=0.0,
@@ -44,7 +42,10 @@ training_args = GRPOConfig(
     num_iterations=1,
     # KL penalty coefficient, default is 0.04, other demos in this repo use lower kl,
     # some people online used smaller kl also https://x.com/abacaj/status/1886497011618197748
-    beta=0.001,
+    # TODO: trying beta 0 to see if it can learn at all even with catastrophic forgetting.
+    # Also base model is just really bad at connect four so kl seems meh
+    beta=0,
+    # beta=0.001,
     max_prompt_length=512,
     max_completion_length=2048,
     per_device_train_batch_size=BATCH_SIZE,
